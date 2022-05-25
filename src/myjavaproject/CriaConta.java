f7238121b29ccd1478a823b42efa64feb202738c
dvdnotfound <contato.dvdsantos@gmail.com>
2022-05-25 01:10:21 -0300

    Myjavaproject init

---
+++ src/myjavaproject/CriaConta.java
@@ -0,0 +1,11 @@
+package myjavaproject;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+		
+	}
+}
---
+++ src/myjavaproject/TestaVariaveis.java
@@ -0,0 +1,12 @@
+package myjavaproject;
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 20;
+		if(idade >= 18) {
+			System.out.println("voce tem mais de 19 anos");
+		}
+	}
+}
